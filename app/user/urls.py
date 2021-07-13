7df7f4b404425afc2f3c72deaa3f084f12bf3ad4
Hima Varshith Reddy Paduri <f20180800@goa.bits-pilani.ac.in>
2021-07-14 00:45:08 +0530

    Added manage user endpoint

--- app/user/urls.py
+++ app/user/urls.py
@@ -4,4 +4,8 @@ from user import views
 app_name ='user'
 
 urlpatterns = [path('create/',views.CreateUserView.as_view(),name ='create'),
-               path('token/', views.CreateTokenView.as_view(), name='token'),]
+               path('token/', views.CreateTokenView.as_view(), name='token'),
+               path('me/', views.ManageUserView.as_view(), name='me'),]
+
+# PUT : updates and replaces the entire object,have to provide all the fields
+# PATCH : only updates the values that we provide
