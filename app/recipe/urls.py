7948b9f019d765f140618ca082ef3fc5e91f5a13
Hima Varshith Reddy Paduri <f20180800@goa.bits-pilani.ac.in>
2021-07-14 14:43:58 +0530

    Added working list endpoint

---
+++ app/recipe/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from recipe import views
+
+#def-router automatically generates/registers all the URLs for our user : /api/recipe/tags/id
+router = DefaultRouter()
+router.register('tags', views.TagViewSet)
+#regsiters the viewset with the router
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls))
+]
+#all urls generated are included in the path
